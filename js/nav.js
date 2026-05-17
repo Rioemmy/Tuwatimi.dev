4df58322edf9fd386ccda2153701793c2c606487
Tuwatimi Emmanuel <tuwatimiemmanuel5@gmail.com>
2026-05-17 16:01:43 +0100

    fix: fixed the toggle button

--- js/nav.js
+++ js/nav.js
@@ -5,9 +5,21 @@
 
 const mobMenu = document.getElementById("mob-menu");
 
-document.getElementById("hamburger").addEventListener("click", () => {
-  mobMenu.classList.add("open");
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", () => {
+  if (!mobMenu.classList.contains("open")) {
+    mobMenu.classList.add("open");
+    hamburger.innerHTML = '✕' ;
+  }
+  else {
+    mobMenu.classList.remove("open");
+    hamburger.innerHTML = '☰' ;
+  }
+
 });
+
+
 
 document.getElementById("mob-close").addEventListener("click", () => {
   mobMenu.classList.remove("open");
